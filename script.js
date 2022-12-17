d89d6252e63fba9ba0428706d2e7e2d34fb46e27
yamazaki2357 <yamazaki2357@gmail.com>
2022-12-17 17:39:12 +0900

    add side-btn

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function() {
   // メイン表示切り替え
   $('.profile-btn').click(function() {
-    console.log('成功');
     $('.import-profile-site').css('display', 'block');
   });
 
@@ -9,10 +8,23 @@ $(function() {
     $('.import-profile-site').css('display', 'none');
   });
 
+
   // スクロールボタン
-    $('#top-btn').click(function() {
+  $('#top-btn').click(function() {
     $('html, body').animate({
       'scrollTop': '0'
     }, 300);
+  });
+
+  // サイドボタン
+  // スマホ版が押されたら
+  $('.small-size-btn').click(function() {
+    $('.site-display').removeClass('pc-size');
+    $('.site-display').addClass('small-size');
+  });
+  // PC版が押されたら
+  $('.pc-size-btn').click(function() {
+    $('.site-display').removeClass('small-size');
+    $('.site-display').addClass('pc-size');
   });
 });
