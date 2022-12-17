16e1b57c0b8d4f931ae12b283e4f8fddec087e64
yamazaki2357 <yamazaki2357@gmail.com>
2022-12-17 10:57:00 +0900

    add scroll btn

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(function() {
+  // メイン表示切り替え
   $('.profile-btn').click(function() {
     console.log('成功');
     $('.import-profile-site').css('display', 'block');
   });
-  
+
   $('.other').click(function() {
     $('.import-profile-site').css('display', 'none');
   });
 
+  // スクロールボタン
+    $('#top-btn').click(function() {
+    $('html, body').animate({
+      'scrollTop': '0'
+    }, 300);
+  });
 });
